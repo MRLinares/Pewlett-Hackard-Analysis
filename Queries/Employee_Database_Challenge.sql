a779a74f43e12adf33bdcdcd4735cf34418e1e59
MRLinares <108758105+MRLinares@users.noreply.github.com>
2022-09-10 09:37:02 -0400

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -84,3 +84,63 @@ WHERE
 	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY
 	emp_no ASC;
+
+-- query to find totals for mentorship eligible titles
+SELECT 
+	titles AS "Titles",
+	COUNT(titles) AS "Totals"
+FROM mentorship_eligibility
+GROUP BY titles
+ORDER BY "Totals" DESC;
+
+
+-- Total current employees by title
+SELECT 
+	t.titles AS "Title",
+	COUNT(DISTINCT (e.emp_no)) AS "Totals"
+FROM employees AS e
+	INNER JOIN
+		titles AS t
+	ON
+		(t.emp_no = e.emp_no)
+GROUP BY t.titles
+ORDER BY "Totals" DESC;
+
+--created a table where all employees born after 
+--the retirement eligible employees would be eligible for mentorship
+SELECT DISTINCT ON(emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	t.titles,
+	de.from_date,
+	de.to_date
+INTO
+	mentorship_ideal
+FROM
+	employees AS e
+		INNER JOIN
+			dept_emp AS de
+		ON
+			e.emp_no = de.emp_no
+		INNER JOIN
+			titles AS t
+		ON
+			e.emp_no = t.emp_no
+WHERE
+	de.to_date = '9999-01-01'
+	AND (e.birth_date > '1955-12-31')
+ORDER BY
+	emp_no ASC;
+
+--mentorship_ideal totals
+SELECT 
+	titles AS "Titles",
+	COUNT(titles) AS "Totals"
+FROM 
+	mentorship_ideal
+GROUP BY
+	titles
+ORDER BY
+	"Totals" DESC;
